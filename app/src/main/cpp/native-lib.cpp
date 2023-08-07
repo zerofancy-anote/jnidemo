b45260c9ec2bfd85e85b97b8f04ec396fafafe40
liuhaixin.zero <liuhaixin.zero@bytedance.com>
2023-08-07 13:42:51 +0800

    JNI的动态注册

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,42 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
+// https://juejin.cn/post/6983321432377786382
 extern "C" JNIEXPORT jstring JNICALL
 Java_top_ntutn_testjni_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+jstring say_hello_2(JNIEnv *env, jobject thiz) {
+    std::string hello2 = "我来自JNI动态注册";
+    return env ->NewStringUTF(hello2.c_str());
+}
+
+static const JNINativeMethod gMethods[] = {
+        "string2FromJNI",
+        "()Ljava/lang/String;",
+        (jstring *) say_hello_2
+};
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    __android_log_print(ANDROID_LOG_INFO, "native", "Jni_OnLoad");
+    JNIEnv *env = nullptr;
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+        __android_log_print(ANDROID_LOG_ERROR, "native", "jni error");
+        return -1;
+    }
+    jclass clazz = env ->FindClass("top/ntutn/testjni/MainActivity");
+    if (!clazz) {
+        __android_log_print(ANDROID_LOG_ERROR, "native", "cannot find class");
+        return -1;
+    }
+    if (env ->RegisterNatives(clazz, gMethods, (sizeof gMethods) / (sizeof gMethods[0]))) {
+        __android_log_print(ANDROID_LOG_ERROR, "native", "register method error");
+        return -1;
+    }
+    return JNI_VERSION_1_6;
 }
--- app/src/main/java/top/ntutn/testjni/MainActivity.kt
+++ app/src/main/java/top/ntutn/testjni/MainActivity.kt
@@ -3,6 +3,7 @@ package top.ntutn.testjni
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.annotation.Keep
 import top.ntutn.testjni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,14 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        binding.sampleText.text = string2FromJNI()
     }
 
     /**
      * A native method that is implemented by the 'testjni' native library,
      * which is packaged with this application.
      */
+    @Keep
     external fun stringFromJNI(): String
+
+    @Keep
+    external fun string2FromJNI(): String
 
     companion object {
         // Used to load the 'testjni' library on application startup.
